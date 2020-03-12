3dd0a8a61a029e33b605730cf3a9454afd2f07f0
injecti0n <rootgebruiker@gmail.com>
2020-03-12 14:20:39 +0100

    puzzle1

---
+++ puzzle_1.js
@@ -0,0 +1,32 @@
+var protected = [ 'S','m','F','W','Y','V','N','y','S','X','B','U','X','0','Z','s','N','G','c','=' ]; // base64 "put all chars in together without comma" ex: 'SmFWYVNySXBUX0ZsNGc='
+
+// begin of hidden function
+var _0x5c29 = ['atob'];
+var _0x121e = function (_0x5c29fc, _0x121e43) {
+    _0x5c29fc = _0x5c29fc - 0x0;
+    var _0xdefa7c = _0x5c29[_0x5c29fc];
+    return _0xdefa7c;
+};
+// end of hidden function 
+
+
+
+
+function PrinterService(x) {
+	return x;
+}
+
+function ______________() {
+    // Use Decryption method in function for SecretMessage > window[_0x121e('0x0')](SecretMessageVariable)
+	PassVariableVariables = PassVariableVariables._____; // convert to string 'SmFWYVNySXBUX0ZsNGc=' find right array method
+	var printIt = _______________________________;
+	return printIt;
+}
+
+// dont touch this function
+function SendStringService(flag) {
+	PassVariableVariables = flag;
+	return flag;
+}
+
+var test = PrinterService(DecrypterService(SendStringService(_____________)))
